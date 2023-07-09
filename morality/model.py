51e5246417e318bdbbfd8ca6458dc715539cc2e3
Rahul Agarwal <111393043+rahulagarwal13@users.noreply.github.com>
2023-07-09 13:55:11 -0700

    Update model.py

--- morality/model.py
+++ morality/model.py
@@ -1,1 +1,28 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Load the dataset
+data = pd.read_csv("morality_dataset.csv")
+
+# Prepare the data
+features = data.drop("morality_level", axis=1)
+labels = data["morality_level"]
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+
+# Create the model
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+
+# Train the model
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+predictions = model.predict(X_test)
+
+# Calculate the accuracy of the model
+accuracy = accuracy_score(y_test, predictions)
+print("Accuracy:", accuracy)
 
